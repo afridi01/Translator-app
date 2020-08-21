38cf6e3f1b5bac97c207dc6b71dfa20164fe5b5d
afridi01 <rejoanafridi93@gmail.com>
2020-08-22 02:57:44 +0600

    next

--- Translator/views.py
+++ Translator/views.py
@@ -7,29 +7,11 @@ def index(request):
     return render(request, 'index.html')
 
 
-def analyze(request):
-    djangoText = request.GET.get('text', 'default')
-    removePuncation = request.GET.get('removePuncation', 'off')
-    print(removePuncation)
-    print(djangoText)
-    # analyzed = djangoText
-    punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
-    analyzed = ""
-    for char in djangoText:
-        if char not in punctuations:
-            analyzed = analyzed + char
-
-        params = {'purpose': 'Removed Punctuations', 'analyzed_text': analyzed}
-        return render(request, 'analyze.html', params)
-
-    else:
-        return HttpResponse('Error')
-
-
 def translator(request):
     text = request.GET.get('text')
     lang = request.GET.get('lang')
     print('text', text, 'lang', lang)
+
     # connect the translator
     translator = Translator()
     # detect language
@@ -38,6 +20,6 @@ def translator(request):
     # translate the text
     translated = translator.translate(text, lang)
     tr = translated.text
-    context = {'translated': tr, 'lang': dt2, 't_lang': lang}
+    context = {'translated': tr, 'lang': dt2, 't_lang':lang }
 
     return render(request, 'transleted.html', context)
